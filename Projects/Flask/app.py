ef01f1dc655a4e2d636fa0b9ce0f95cd496f5438
ajaysureshcodes <156284973+ajaysureshcodes@users.noreply.github.com>
2024-04-23 11:00:49 +0530

    Add files via upload

---
+++ Projects/Flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import pickle
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+model = pickle.load(open("purchase_model.pkl", "rb"))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    age = int(request.form["age"])
+    salary = float(request.form["salary"])
+    data = [[age, salary]]
+    sc_data = scaler.transform(data)
+    print(sc_data)
+    prediction = model.predict(sc_data)[0]
+    if prediction == 1:
+        outcome = "Likely to purchase the product"  
+    else:
+        outcome = "Unlikely to purchase the product"  
+    return render_template("index.html", result=outcome)
+if __name__ == "__main__":
+    app.run(debug=True)
